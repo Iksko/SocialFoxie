b66f554efcc44e7bbf8f737d493cf881c78f1661
Iksko <sykorka.t@email.cz>
2023-03-06 19:38:57 +0100

    Header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*HAMMENU*/
 const hamburger = document.getElementById("hamburger-menu")
 const navbar = document.getElementById("mobile-navbar")
-
+/*OPEN AND CLOSE*/
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("is-active")
     navbar.classList.toggle("is-active")
@@ -10,15 +10,15 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("is-active")
     navbar.classList.remove("is-active")
+    categoriesUlLinks.classList.remove("is-active")
 }))
 
-const exploreMore = document.getElementById("explore-more")
-const exploreMoreLinks = document.getElementById("explore-more-links")
-const categories = document.getElementById("category")
+/*CATEGORIES DROP-DOWN ICON*/
+const categories = document.getElementById("categoriesBtn")
+const categoriesUlLinks = document.getElementById("categoriesLinks")
+const chevronRight = document.getElementById("chevronRight")
 
-exploreMore.addEventListener("click", () => {
-    exploreMoreLinks.classList.toggle("is-active")
-})
-category.addEventListener("click", () => {
-    exploreMoreLinks.classList.toggle("is-active")
+categories.addEventListener("click", () => {
+    categoriesUlLinks.classList.toggle("is-active")
+    chevronRight.classList.toggle("is-active")
 })
